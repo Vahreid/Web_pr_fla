65a46ad3b90cd950fb0fa7fde881b2fcc7b1a422
Ekaterina3456 <130658679+Ekaterina3456@users.noreply.github.com>
2025-01-19 19:41:48 +1000

    Дохера изменений

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
@@ -6,13 +6,20 @@ menu = ["Установка", "Первое приложение", "Обратн
 
 @app.route("/")
 def index():
+    print(url_for('index'))
     return render_template('index.html', menu=menu)
 
 @app.route("/about")
 def about():
+    print(url_for ('about'))
     return render_template('about.html', title="О сайте", menu=menu)
 
 
+
+"""
+with app.test_reqest_context():
+    print(url_for ('index'))
+"""
 
 
 if __name__ == "__main__":
