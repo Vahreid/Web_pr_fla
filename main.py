3d1ab43c2f05d249d08b2379ec144582be6e47ed
Vahreid <138991090+Vahreid@users.noreply.github.com>
2025-01-19 11:49:15 +0300

    Commit 1 Этап 0

--- main.py
+++ main.py
@@ -2,13 +2,21 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+menu = ["Установка", "Первое приложение", "Обратная связь"]
+
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template('index.html', menu=menu)
 
 @app.route("/about")
 def about():
-    return  "<h1>Про Flack<h1>"
+    return render_template('about.html', title="О сайте", menu=menu)
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+  return f'User {username}'
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
