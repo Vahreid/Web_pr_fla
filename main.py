49740f558ab101c89b57b1d09829b228731be517
Ekaterina3456 <130658679+Ekaterina3456@users.noreply.github.com>
2025-01-21 21:30:12 +1000

    добавление гиперссылки Обратная связь

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
 
@@ -19,6 +19,12 @@ def about():
 @app.route('/profile/<path:username>')
 def profile(username):
     return f'Пользователь: {username}'
+
+@app.route("/contact", methods=["POST", "GET"])
+def contact():
+    if request.method == 'POST':
+        print(request.form)
+    return render_template('contact.html', title = 'Обратная связь', menu=menu)
 
 """
 with app.test_reqest_context():
